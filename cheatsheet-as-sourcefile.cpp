f812df52d46998eda95eefabd4a7a41676d24a6d
Davis Amankwa <amankwa.davis@gmail.com>
2020-05-06 22:08:58 +0200

    Update cheatsheet-as-sourcefile.cpp
    Testing push o github desktop

--- cheatsheet-as-sourcefile.cpp
+++ cheatsheet-as-sourcefile.cpp
@@ -528,3 +528,7 @@ future<int> fut =         // result of async function
         async(launch::async, fib, 4); // start async function in other thread
 // do some other work 
 cout << fut.get();        // get result of async function. Wait if needed.
+
+
+
+//testing push
